7a0e64bb08abd4f4e2c6d9bbb3bdd2c5077f7c35
看看太阳 <aa944589323@gmail.com>
2025-01-03 17:27:36 +0800

    secondCommitFromJobComputer

---
+++ AnnotationDemo/src/com/learn/cwb/AbstractDemo.java
@@ -0,0 +1,47 @@
+package com.learn.cwb;
+//author:cwb 2025/1/3 17:12
+//Administrator
+//                          _ooOoo_                               //
+//                         o8888888o                              //
+//                         88" . "88                              //
+//                         (| ^_^ |)                              //
+//                         O\  =  /O                              //
+//                      ____/`---'\____                           //
+//                    .'  \\|     |//  `.                         //
+//                   /  \\|||  :  |||//  \                        //
+//                  /  _||||| -:- |||||-  \                       //
+//                  |   | \\\  -  /// |   |                       //
+//                  | \_|  ''\---/''  |   |                       //
+//                  \  .-\__  `-`  ___/-. /                       //
+//                ___`. .'  /--.--\  `. . ___                     //
+//              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+//            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+//            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+//      ========`-.____`-.___\_____/___.-`____.-'========         //
+//                           `=---='                              //
+//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+//              佛祖保佑       永无BUG     永不修改                  //
+
+ abstract class AbstractDemo {
+     private  int id ;
+     public  int name ;
+     public  void normalMethod()
+     {
+         System.out.println("ccc");
+     }
+    public abstract  void   Method01();
+}
+
+ class NormalClass  extends  AbstractDemo{
+
+    @Override
+    public void Method01() {
+        this.name = 1;
+        super.name =2;
+    }
+
+     @Override
+     public void normalMethod() {
+         super.normalMethod();
+     }
+ }
--- UdpDemo/src/com/company/cwb/ServerDemo.java
+++ UdpDemo/src/com/company/cwb/ServerDemo.java
@@ -68,6 +68,5 @@ public class ServerDemo {
             e.printStackTrace();
         }
 
-
     }
 }
